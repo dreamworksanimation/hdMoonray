33366e092c145c0e774c499449158f8d26f406c8
scottc <scott.cegielski@dreamworks.com>
2023-08-01 20:50:50 -0700

    Update hdMoonray to use the newly released moonbase packages versions and bump its version.

--- package.py
+++ package.py
@@ -13,7 +13,7 @@ if 'early' not in locals() or not callable(early):
 
 @early()
 def version():
-    _version = '3.7'
+    _version = '3.8'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -63,13 +63,13 @@ sconsTargets = {
 }
 
 requires = [
-    'moonray-14.7',
-    'moonshine_dwa-11.7',
-    'moonshine-11.7',
-    'mcrt_computation-12.7',
+    'moonray-14.8',
+    'moonshine_dwa-11.8',
+    'moonshine-11.8',
+    'mcrt_computation-12.8',
     'arras4_core-4.10',
     'mcrt_messages-11.3',
-    'mcrt_dataio-12.7',
+    'mcrt_dataio-12.8',
     'mkl',
     'openimageio-2.3.20.0.x',
 ]
